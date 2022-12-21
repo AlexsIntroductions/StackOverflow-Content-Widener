372da7307877d388923f05cded510d048517d8a2
AlexsIntroductions <alexsintroductions@gmail.com>
2022-12-21 14:47:44 -0500

    Added Functionality and removed background worker

--- popup.js
+++ popup.js
@@ -1,22 +1,28 @@
-let changeColor = document.getElementById("changeColor");
+let widenButton = document.getElementById("widenButton");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+//testing document elements to get
+let sidebar = document.getElementById("sidebar");
+let content = document.getElementById("content");
+let mainbar = document.getElementById("mainbar");
+
+//How to widen StackOverflow Content
+//delete sidebar element
+//content gets max width removed and width changed to 100%
+//mainbar will be removed
+
+widenButton.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: widenContent,
+  });
 });
 
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: setPageBackgroundColor,
-    });
-  });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
+// The body of this function will be executed as a content script inside the
+// current page
+function widenContent() {
+  sidebar.remove();
+  mainbar.style.maxWidth = '100%';
+  mainbar.style.width = '100%';
+}
