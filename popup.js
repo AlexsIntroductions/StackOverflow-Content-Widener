b6886ad70a255324a120ecf4dd959459ccedebe6
AlexsIntroductions <alexsintroductions@gmail.com>
2022-12-21 15:38:45 -0500

    Added some testing functions and prep for release

--- popup.js
+++ popup.js
@@ -1,14 +1,15 @@
 let widenButton = document.getElementById("widenButton");
 
 //testing document elements to get
-let sidebar = document.getElementById("sidebar");
-let content = document.getElementById("content");
-let mainbar = document.getElementById("mainbar");
+let content = document.getElementById('content'); 
+let container = document.getElementById('left-sidebar');
+let sidebar = document.getElementById('sidebar');
+let mainbar = document.getElementById('mainbar');
 
 //How to widen StackOverflow Content
-//delete sidebar element
-//content gets max width removed and width changed to 100%
-//mainbar will be removed
+  //delete sidebar element
+  //content gets max width removed and width changed to 100%
+  //mainbar will be removed
 
 widenButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -19,9 +20,8 @@ widenButton.addEventListener("click", async () => {
   });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
 function widenContent() {
+  console.log("removed");
   sidebar.remove();
   mainbar.style.maxWidth = '100%';
   mainbar.style.width = '100%';
